e31b99b83f408b9f1aecf569470aaca23934642d
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-07-08 13:49:32 +0200

    add directory layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Navbar from "~/component/navbar.vue";
+</script>
+
+<template>
+
+
+  <Navbar class="pt-20"/>
+
+  <div class="mt-14 ">
+    <slot />
+  </div>
+</template>
