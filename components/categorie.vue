464118dae5da714ae5265e58de9bb301efd76cf3
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-07-09 14:28:44 +0200

    fix navbar

--- components/categorie.vue
+++ components/categorie.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
-
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 </script>
 
 <template>
+
+  <div class="grid grid-cols-3" >
+
+
+
+  </div>
+
+
+
 
 </template>
 
