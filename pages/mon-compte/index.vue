43650431386f6addf721bc5789836b4d12ad887f
Paul Niggli <58045354+Rommel25@users.noreply.github.com>
2024-07-09 16:58:05 +0200

    commit ça mon copaing

--- pages/mon-compte/index.vue
+++ pages/mon-compte/index.vue
@@ -3,6 +3,7 @@ import { ref, reactive, onMounted } from 'vue';
 import { z } from 'zod';
 import type { FormSubmitEvent } from '#ui/types';
 import { fetchWithoutBody, fetchWithBody } from '~/utils/utils.ts'; // Mettez à jour le chemin vers vos fonctions
+import { useUserStore } from "~/stores/UserStore.ts"
 
 const loading = ref(false);
 const message = ref('');
@@ -22,21 +23,29 @@ const state = reactive<Schema>({
   last_name: '',
   first_name: '',
   age: 0,
-  phone: '',
+  phoneNumber: '',
   location: '',
-  description: ''
+  languages: '',
+  profile_picture: '',
+  credits: 0,
 });
 
-const userId = ref(''); // Assurez-vous que l'ID est disponible
 
+const userStore = useUserStore();
+const { user, isAuthenticated } = storeToRefs(userStore); // Assurez-vous que l'ID est disponible
 async function fetchUserData() {
+  console.log(user)
+  console.log(user.value)
   try {
     loading.value = true;
-    const user = await fetchWithoutBody('users/' + "668cf096042d278f06ea5214", 'GET');
-    console.log("USER:"+ user.name); 
+    const user = await fetchWithoutBody('users/' + userStore.user.googleId, 'GET');
     if (user) {
       // Update all properties of state with the fetched data
-      state.last_name = user.name
+      state.first_name = user.name
+      state.languages = user.languages
+      state.location = user.location
+      state.profile_picture = user.profile_picture
+      state.credits = user.credits
       console.log('User data updated successfully');
     }
   } catch (e) {
@@ -48,24 +57,21 @@ async function fetchUserData() {
 
 onMounted(async () => {
   const userStorage = localStorage.getItem('user');
-  if (userStorage) {
-    const user = JSON.parse(userStorage);
-    userId.value = user.id; // Make sure the ID is included in the storage
+  
+  if (userStore.user.name !== "") {
     await fetchUserData();
-    console.log(state); // This will now log the updated state with real data
   }
 });
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   try {
     const parsedData = schema.parse(state);
-    const response = await fetchWithBody('users/' + "668cf096042d278f06ea5214", 'GET', parsedData);
+    const response = await fetchWithBody('users/' + "668cf096042d278f06ea5214", 'PATCH', parsedData);
     console.log('Success:', response);
   } catch (e) {
     console.error('Failed to update user data', e);
   }
 }
-fetchUserData()
 </script>
 
 <template>
@@ -76,7 +82,7 @@ fetchUserData()
         <UCard class="w-full flex flex-col items-center ">
           <h3 class="font-bold w-full text-center pb-3 ">Photo de profil 😊</h3>
           <NuxtImg
-              src="https://via.placeholder.com/200"
+              :src="state.profile_picture"
               alt="Profile picture"
               class="rounded-[15%]  w-60 h-60"
           />
@@ -105,20 +111,14 @@ fetchUserData()
     <UFormGroup label="Name" name="name">
       <UInput v-model="state.first_name" />
     </UFormGroup>
-    <UFormGroup label="Last Name" name="last_name">
-      <UInput v-model="state.last_name"/>
-    </UFormGroup>
     <UFormGroup label="Age" name="age">
       <UInput type="number" v-model="state.age"/>
     </UFormGroup>
     <UFormGroup label="Phone" name="phone">
-      <UInput v-model="state.phone" />
+      <UInput v-model="state.languages" />
     </UFormGroup>
     <UFormGroup label="Location" name="location">
       <UInput v-model="state.location" size="xl" />
-    </UFormGroup>
-    <UFormGroup label="Description" name="description">
-      <UTextarea v-model="state.description" resize />
     </UFormGroup>
 
             <UButton type="submit" class="w-fit px-4 rounded-full bg-gray-500 hover:bg-blue-500 ">Modifier</UButton>
@@ -144,7 +144,7 @@ fetchUserData()
         </UCard>
         <UCard class="w-full flex flex-col items-center ">
           <h3 class="font-bold w-full text-center pb-3 ">Crédit restant(s) 💰</h3>
-          <p class="text-center">Vous avez <span class="font-bold">10</span> crédits restants</p>
+          <p class="text-center">Vous avez <span class="font-bold">{{state.credits}}</span> crédits restants</p>
         </UCard>
       </div>
     </section>
