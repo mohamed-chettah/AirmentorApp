e6b5ae5c21b7da058bc2fc0a8412d8e3db1b906b
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-07-09 15:01:38 +0200

    fix

--- components/hero-section.vue
+++ components/hero-section.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+const stats = [
+  { id: 1, name: 'Sessions de mentorat par mois', value: '5000+' },
+  { id: 2, name: 'Mentors disponibles', value: '1200' },
+  { id: 3, name: 'Taux de satisfaction', value: '98%' },
+]
 
 </script>
 
@@ -19,10 +24,21 @@
             Des mentors experts pour <br> résoudre tous vos problèmes.
           </h1>
 
-          <div class="flex flex-col font-bold gap-4 mt-10">
+          <div class="flex flex-col font-bold gap-4 mt-14">
             <p class="text-xl">🔥 Des mentors partout en France </p>
             <p  class="text-xl">🎓 Pour toutes les matières de la tech </p>
             <p  class="text-xl"> 💪 Pour tous les niveaux et budgets </p>
+          </div>
+
+          <div class="mt-20">
+            <div class="max-w-4xl">
+              <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+                <div v-for="stat in stats" :key="stat.id" class="mx-auto flex max-w-xs flex-col gap-y-4">
+                  <dt class="text-base leading-7 text-gray-600">{{ stat.name }}</dt>
+                  <dd class="order-first font-semibold tracking-tight text-gray-900 text-2xl">{{ stat.value }}</dd>
+                </div>
+              </dl>
+            </div>
           </div>
         </div>
 
