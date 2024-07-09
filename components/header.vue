0d37b6281d59e0fee12ac3092b71c4319e438a8f
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-07-09 15:18:52 +0200

    fix

--- components/header.vue
+++ components/header.vue
@@ -91,6 +91,14 @@ async function logout() {
         <DisclosureButton as="a" href="#" class="block rounded-md bg-primary hover:opacity-65 px-3 py-2 text-base font-medium text-white">Annonces</DisclosureButton>
       </div>
       <div class="border-t border-gray-700 pb-3 pt-4">
+
+        <div class="flex justify-center flex-row" v-if="!isAuthenticated">
+
+          <UButton @click="authenticate" class="bg-gray-100 text-primary rounded-xl hover:text-white xl:text-sm text-xs  border-[1px] border-primary" variant="solid">  <img class="h-6" src="../public/img/google.png" alt="Google logo" /> Se connecter</UButton>
+        </div>
+
+        <div v-else >
+
         <div class="flex items-center px-5">
           <div class="flex-shrink-0">
             <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
@@ -105,10 +113,12 @@ async function logout() {
             <BellIcon class="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <div class="mt-3 space-y-1 px-2">
-          <DisclosureButton as="a" href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</DisclosureButton>
-          <DisclosureButton as="a" href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</DisclosureButton>
-          <DisclosureButton as="a" href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</DisclosureButton>
+          <div class="mt-3 space-y-1 px-2">
+            <DisclosureButton as="a" href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</DisclosureButton>
+            <DisclosureButton as="a" href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</DisclosureButton>
+            <DisclosureButton as="a" href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</DisclosureButton>
+          </div>
+
         </div>
       </div>
     </DisclosurePanel>
