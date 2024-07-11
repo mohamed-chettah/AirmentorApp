b0d6ec5cc7310103449e9f76078ff64be2861255
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-07-11 15:19:11 +0200

    FIX SKILL

--- types/SkillType.ts
+++ types/SkillType.ts
@@ -1,5 +1,14 @@
+import { z } from "zod";
+
 export type SkillType = {
     title: string;
-    categories: [] | string;
-    _id? : string;
+    categories: string;
+    _id?: string;
 };
+
+export const SkillSchema = z.object({
+    _id: z.string().optional(),
+    title: z.string().min(1, 'La compétence est obligatoire'),
+    categories: z.string().min(1, 'La catégorie est obligatoire'),
+});
+
