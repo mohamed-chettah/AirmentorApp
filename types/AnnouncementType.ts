6ceaa3d322540c75d591feef7fec627a57427b62
Cihaan <cihankafadar1@gmail.com>
2024-07-11 15:00:20 +0200

    upupup

--- types/AnnouncementType.ts
+++ types/AnnouncementType.ts
@@ -1,24 +1,23 @@
-import type {UserType} from "~/types/UserType";
-import {z} from 'zod';
+import { z } from "zod";
+import type { UserType } from "~/types/UserType";
 
 export type AnnouncementType = {
-    _id: string;
-    title: string;
-    description: string;
-    picture: string;
-    skills: Object[];
-    is_activate: boolean;
-    review: Object[];
-    createdBy: UserType;
-}
+  _id: string;
+  title: string;
+  description: string;
+  picture: string;
+  skills: Object[];
+  is_activate: boolean;
+  createdBy: UserType;
+};
 
 export const AnnouncementSchema = z.object({
-    _id: z.string().optional(),
-    title: z.string().min(1, 'Title is required'),
-    description: z.string().min(1, 'Description is required'),
-    picture: z.string().url('Must be a valid URL'),
-    skills: z.array(z.object({})),
-    is_activate: z.boolean(),
-    review: z.array(z.object({})),
-    createdBy: z.object({})
+  _id: z.string().optional(),
+  title: z.string().min(1, "Title is required"),
+  description: z.string().min(1, "Description is required"),
+  picture: z.string().url("Must be a valid URL"),
+  skills: z.array(z.object({})),
+  is_activate: z.boolean(),
+  review: z.array(z.object({})),
+  createdBy: z.object({}),
 });
--- types/UserType.ts
+++ types/UserType.ts
@@ -1,15 +1,23 @@
+export type Review = {
+  grade: number;
+  description: string;
+  reviewer: UserType;
+  reviewed: UserType;
+};
+
 export type UserType = {
-    _id: string;
-    name: string;
-    email: string;
-    phoneNumber: string;
-    place: string;
-    password: string;
-    profile_picture: string;
-    grade: number;
-    credits: number;
-    description: string;
-    languages: string[];
-    googleId: string;
-    role: string
+  _id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  place: string;
+  password: string;
+  profile_picture: string;
+  grade: number;
+  credits: number;
+  description: string;
+  languages: string[];
+  googleId: string;
+  role: string;
+  reviews: Review[];
 };
