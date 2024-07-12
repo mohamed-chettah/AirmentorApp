856b1f9218e57251f0b3aee212e9d42feaeaf9ef
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-07-12 11:21:25 +0200

    fix

--- pages/annonces/[id].vue
+++ pages/annonces/[id].vue
@@ -171,7 +171,7 @@
     <div class="w-1/3">
       <UCard>
         <div class="flex flex-col gap-4 p-4">
-          <NuxtImg src="/img/main-picture.png" class="w-full h-48 object-cover rounded-md" />
+          <NuxtImg :src="announcement.picture" class="w-full h-48 object-cover rounded-md" />
           <h3 class="font-bold text-xl text-gray-800">{{ announcement.createdBy.name }}</h3>
           <div class="flex gap-4 items-center">
             <div class="flex gap-1">
@@ -205,7 +205,7 @@
             </UButton>
 
           </div>
-          <UButton v-if="isRegistered && announcement.createdBy._id !== userStore.user._id"
+          <UButton v-if="isRegistered || announcement.createdBy._id == userStore.user._id"
             @click="displayCamera = !displayCamera">
             <UIcon name="i-heroicons-video-camera" class="text-2xl " />
             Facetime
